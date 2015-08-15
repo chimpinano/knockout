1926e1e17d947826ebdd89a1034a3f8c07ee1ca7
Тодор Господинов <gospodinov.web@gmail.com>
2015-08-15 22:00:44 +0300

    adding products to cart (sweet)

--- cart-tutorial/src/app.js
+++ cart-tutorial/src/app.js
@@ -1,1 +1,33 @@
-ko.applyBindings(categories);
+ko.bindingHandlers.addToCart = {
+	init: function(el, valAccessor, allBindings, vm, bindingContext){
+		$(el).on('click', function(){
+			if(cart.itemExists(vm)){
+				cart.items()[cart.items.indexOf(vm)].quantity++;
+			} else {
+				vm.quantity = 1;
+				cart.items.push(vm);
+			}
+			
+		});
+	}
+}
+
+var cart = {
+	items: ko.observableArray(),
+	itemExists: function(item){
+		var self = this;
+		if( self.items.indexOf(item) !== -1 ){
+			console.log('exists');
+			return true;
+		} else {
+			console.log('dont exists');
+			return false;
+		}
+	}
+};
+
+
+
+ko.applyBindings(categories, document.getElementById('bs-example-navbar-collapse-1'));
+ko.applyBindings(products, document.getElementById('isotopeBody'));
+ko.applyBindings(cart, document.getElementById('cartProductsWrapper'))
--- cart-tutorial/src/data/products.js
+++ cart-tutorial/src/data/products.js
@@ -3,5 +3,10 @@ var products = [
 		"name": "Example Product",
 		"price": 123.00,
 		"image": "http://www.armenasadorian.com/wp-content/uploads/2012/03/fashion_product_photography-1.jpg"
+	},
+	{
+		"name": "Example Product",
+		"price": 12.00,
+		"image": "http://www.highsnobiety.com/news/wp-content/uploads/2010/11/lanvin-hm-fashion-men-products-29.jpg"
 	}
 ]
