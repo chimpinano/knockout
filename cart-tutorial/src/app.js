63b45ca868cae1ba2d05772894dd2545613aedd0
Тодор Господинов <gospodinov.web@gmail.com>
2015-08-17 12:25:53 +0300

    some stylings added

--- cart-tutorial/src/app.js
+++ cart-tutorial/src/app.js
@@ -1,17 +1,7 @@
 ko.bindingHandlers.addToCart = {
     init: function (el, valAccessor, allBindings, vm, bindingContext) {
         $(el).on('click', function () {
-            var exists = ko.utils.arrayFirst(cart.items(), function (item) {
-                return vm.id === item.product().id;
-            });
-
-            if (!exists) {
-                var node = new CartNode(vm);
-                cart.items().push(node);
-            } else {
-                var qtty = cart.items()[cart.items().indexOf(exists)].quantity();
-                cart.items()[cart.items().indexOf(exists)].quantity(qtty + 1);
-            }
+            cart.addOrUpdateItem(vm);
         });
     }
 };
@@ -20,22 +10,47 @@ function CartNode(product) {
     var self = this;
     self.quantity = ko.observable(1);
     self.product = ko.observable(product);
-    self.subtotal = ko.computed(function(){
+    self.subtotal = ko.computed(function () {
         return self.product().price * self.quantity();
     });
 }
 
 function Cart() {
+
     var self = this;
+
     self.isVisible = ko.observable(true);
-    self.items = ko.observableArray([]);
-    self.itemExists = function (item) {
-        return ( this.items().indexOf(item) !== -1 );
+    self.items = ko.observableArray();
+
+    self.addOrUpdateItem = function (vm) {
+        var exists = ko.utils.arrayFirst(cart.items(), function (item) {
+            return vm.id === item.product().id;
+        });
+
+        if (!exists) {
+            var node = new CartNode(vm);
+            cart.items().push(node);
+        } else {
+            var qtty = cart.items()[cart.items().indexOf(exists)].quantity();
+            cart.items()[cart.items().indexOf(exists)].quantity(qtty + 1);
+        }
     };
+    self.subtotal = ko.pureComputed(function () {
+        var total = 123;
+        ko.utils.arrayForEach(self.items(), function(item){
+            console.log(item.subtotal());
+        });
+        console.log('evaluated: ' + total);
+        return total;
+    });
+    self.items.subscribe(function(){
+        console.log('subscribe changes');
+    })
 }
+
 
 var cart = new Cart();
 
 ko.applyBindings(categories, document.getElementById('bs-example-navbar-collapse-1'));
 ko.applyBindings(products, document.getElementById('isotopeBody'));
-ko.applyBindings(cart, document.getElementById('cartProductsWrapper'))
+ko.applyBindings(cart, document.getElementById('cartProductsWrapper'));
--- cart-tutorial/src/data/products.js
+++ cart-tutorial/src/data/products.js
@@ -9,6 +9,6 @@ var products = [
         "id": 2,
         "name": "Example Product",
         "price": 123.00,
-        "image": "http://www.armenasadorian.com/wp-content/uploads/2012/03/fashion_product_photography-1.jpg"
+        "image": "http://www.maadesigns.co.uk/portfolio/3d-product-modeling/images/04-a.jpg"
     }
 ]
